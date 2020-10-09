cff25acc4a6aaf05e7f165acaee6e691060c421a
ronaldhuamani <fredyronaldhuamani@gmail.com>
2020-10-09 02:28:04 +0000

    ejercicio4

---
+++ .vscode/ejercicio4.py
@@ -0,0 +1,16 @@
+def costoydescuento():
+    #datos de entrada
+    costo =float(input("ingrese el precio y el descuento : "))
+    #proceso
+    if costo > 0:
+
+        total=costo*200
+    if 200<=costo:
+            total = total*0.15
+    elif 100<costo <200:
+            total = total*0.12
+    elif 100>costo:
+            total = total*0.10
+    #datos de salida
+    print("el total a pagar es: " , format(total))
+costoydescuento()    
